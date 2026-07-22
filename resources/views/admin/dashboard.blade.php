c14ab6ffa213ba26ef18329bfd47840b6f42d5ad
ajay kumar <ajaykumarwrs1997@gmail.com>
2026-07-22 15:54:48 +0530

    j

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -138,7 +138,7 @@
 
                                 @if(!empty($row['state']))
                                     <small class="text-muted">
-                                        <strong>State:</strong> {{ $row['state'] }} - {{ $row['district'] }}
+                                        <strong>State:</strong> {{ $row['state'] }} - {{ $row['city'] }}
                                     </small><br>
                                 @endif
 
